8281363d90f6bf67936abc6ff7d8b435ed4dde23
yasmiiiiin <nadia.hbr@gmail.com>
2017-11-15 10:10:50 +0100

    delete extra files

--- assets/js/insertQ10.js
+++
@@ -1,9 +0,0 @@
-function insertQ10() {
-    $.ajax({
-        type: "post",
-        url: "<?php echo base_url(); ?>index.php/home/set_answers/",
-        data: {"answer_body": Q10, "question_id": question_id, "user_id": user_id},
-        dataType: "json"
-    });
-
-}
